21c191d9225b794e8779946f02ff39229cb73bec
miguelaarrones <miguelaarrones@icloud.com>
2023-12-05 16:12:52 +0100

    Added plate complete visual

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,9 +1,16 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddEventArgs> OnIngredientAdded;
+    public class OnIngredientAddEventArgs : EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField] private List<KitchenObjectSO> validKitchenObjectSOList;
 
     private List<KitchenObjectSO> kitchenObjectSOList;
@@ -15,6 +22,7 @@ public class PlateKitchenObject : KitchenObject
 
     public bool TryAddIngredient(KitchenObjectSO kitchenObjectSO)
     {
+        Debug.Log(kitchenObjectSO.objectName);
         if (!validKitchenObjectSOList.Contains(kitchenObjectSO))
         {
             // Not a valid ingredient.
@@ -27,6 +35,12 @@ public class PlateKitchenObject : KitchenObject
         } else
         {
             kitchenObjectSOList.Add(kitchenObjectSO);
+
+            OnIngredientAdded?.Invoke(this, new OnIngredientAddEventArgs
+            {
+                kitchenObjectSO = kitchenObjectSO
+            });
+
             return true;
         }
         
