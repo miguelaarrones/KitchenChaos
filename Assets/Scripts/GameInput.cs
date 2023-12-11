c9d694fafb67edf0e1af2e0b51ea8a1b97547737
miguelaarrones <miguelaarrones@icloud.com>
2023-12-11 19:57:01 +0100

    Added a pause menu

--- Assets/Scripts/GameInput.cs
+++ Assets/Scripts/GameInput.cs
@@ -5,8 +5,11 @@ using UnityEngine;
 
 public class GameInput : MonoBehaviour
 {
+    public static GameInput Instance { get; private set; } 
+
     public event EventHandler OnInteractAction;
     public event EventHandler OnInteractAlternateAction;
+    public event EventHandler OnPauseAction;
 
     private PlayerInputActions playerInputActions;
 
@@ -16,7 +19,26 @@ public class GameInput : MonoBehaviour
         playerInputActions.Player.Enable();
 
         playerInputActions.Player.Interact.performed += Interact_performed;
-        playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed; ;
+        playerInputActions.Player.InteractAlternate.performed += InteractAlternate_performed;
+
+        playerInputActions.Player.Pause.performed += Pause_performed;
+
+        Instance = this;
+    }
+
+    private void OnDestroy()
+    {
+        playerInputActions.Player.Interact.performed -= Interact_performed;
+        playerInputActions.Player.InteractAlternate.performed -= InteractAlternate_performed;
+
+        playerInputActions.Player.Pause.performed -= Pause_performed;
+
+        playerInputActions.Dispose();
+    }
+
+    private void Pause_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
+    {
+        OnPauseAction?.Invoke(this, EventArgs.Empty);
     }
 
     private void InteractAlternate_performed(UnityEngine.InputSystem.InputAction.CallbackContext obj)
